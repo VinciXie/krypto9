a3b307a8bea8b5df5e61467944195005f300c6c5
VinciXie <szqq602@163.com>
2019-12-16 15:34:29 +0800

    一些适配问题

--- src/index.js
+++ src/index.js
@@ -3,60 +3,6 @@ const canvasApp = require('./sphere')
 import './css/clients.css'
 import './css/style.css'
 
-// 第一屏的点的生成
-function rotatingPoints() {
-    var svg1 = d3.select('#points');
-
-    const width = 450;
-    const height = 450;
-
-    svg1.attr("width", width);
-    svg1.attr("height", height);
-
-
-    const pointR = 1;
-    const pointBg = 'black';
-    const circleR = width / 2 - 50;
-    const center = [width / 2, height / 2]
-
-    const pointNumber = 30;
-
-    var data = [];
-
-    for (let index = 0; index < pointNumber; index++) {
-        const angle = 2 * Math.PI * index / pointNumber
-        data.push({
-            x: center[0] + circleR * Math.cos(angle),
-            y: center[1] + circleR * Math.sin(angle),
-            r: pointR,
-            c: pointBg
-        })
-    }
-
-    var circles = svg1
-        .selectAll('circle')
-        .data(data);
-
-    circles
-        .enter()
-        .append('circle')
-        .attr('cx', function (d) {
-            return d.x;
-        })
-        .attr('cy', function (d) {
-            return d.y;
-        })
-        .attr('fill', function (d) {
-            return d.c;
-        })
-        .attr('r', function (d) {
-            return d.r;
-        });
-
-}
-
-// rotatingPoints()
-
 window.addEventListener("load", canvasApp, false);
 
 const myPage = new window.fullpage('#fullpage', {
@@ -65,19 +11,21 @@ const myPage = new window.fullpage('#fullpage', {
         // console.log('destination.index', destination.index);
         if (origin.index !== destination.index) {
             if (destination.index === 1) {
-                // window.$('.top-logo').addClass('black');
+                window.setTimeout(function () {
+                    window.$('.top-logo').addClass('black');
+                }, 500)
             } else {
                 window.$('.top-logo').removeClass('black');
             }
         }
     },
-    afterLoad: function (origin, destination, direction) {
-        console.log('origin.index', origin.index);
-        console.log('destination.index', destination.index);
-        if (origin.index === 0 && destination.index === 1) {
-            window.$('.top-logo').addClass('black');
-        }
-    }
+    // afterLoad: function (origin, destination, direction) {
+    //     console.log('origin.index', origin.index);
+    //     console.log('destination.index', destination.index);
+    //     if (origin.index === 0 && destination.index === 1) {
+    //         window.$('.top-logo').addClass('black');
+    //     }
+    // }
 });
 
 // console.log('myPage', myPage);
